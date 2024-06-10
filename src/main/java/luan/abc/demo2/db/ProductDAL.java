3338f15a76f0ab86c3e95d4946678fabdfcf4f01
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-06-10 19:49:08 +0700

    update DAL product

---
+++ src/main/java/luan/abc/demo2/db/ProductDAL.java
@@ -0,0 +1,37 @@
+package luan.abc.demo2.db;
+
+import luan.abc.demo2.models.Category;
+import luan.abc.demo2.models.Product;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ProductDAL extends DB {
+    public ProductDAL() {
+        connect();
+    }
+
+    public ArrayList<Product> getList() {
+        String sql = "SELECT * FROM products";
+        try {
+            Statement stm = connection.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            ArrayList<Product> products = new ArrayList<>();
+            while (rs.next()) {
+                Product product = new Product();
+                product.setId(rs.getInt("id"));
+                product.setName(rs.getString("name"));
+                product.setPrice(rs.getFloat("price"));
+                product.setDescription(rs.getString("description"));
+                product.setPath(rs.getString("path"));
+                products.add(product);
+            }
+            return products;
+        } catch (Exception e) {
+
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/luan/abc/demo2/models/Product.java
@@ -0,0 +1,49 @@
+package luan.abc.demo2.models;
+
+public class Product {
+    int id;
+    String name;
+    float price;
+    String description;
+    String path;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
